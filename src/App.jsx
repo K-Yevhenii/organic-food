25fe32862c24edfa57ca814a60d2eddb3523d0cf
kornyakoff <dr.kornyakoff@gmail.com>
2025-06-11 11:37:00 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <header className="header">
+        <div className='header__logo'>
+          <div className="header__logo-ellipse">
+            {/* Logo */}
+          </div>
+            <h3 className='header__text-logo'>Logo</h3>
+        </div>
+
+        <nav className="header__menu-nav nav">
+          <ul className="nav__contacts">
+            <li className="nav__item"><a className='nav__contact' href="#">About</a></li>
+            <li className="nav__item"><a className='nav__contact' href="#">Services</a></li>
+            <li className="nav__item"><a className='nav__contact' href="#">Team</a></li>
+            <li className="nav__item"><a className='nav__contact' href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="hero">
+        {/* Контент головної секції */}
+      </main>
+    </div>
   )
 }
 
